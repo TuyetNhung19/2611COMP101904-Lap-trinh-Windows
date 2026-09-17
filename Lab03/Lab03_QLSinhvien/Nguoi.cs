a286a6903ff267aa77abb2a00e434554e6fd494a
Võ Ngọc Tuyết Nhung <146346927+TuyetNhung19@users.noreply.github.com>
2026-09-17 15:45:34 +0700

    Add Lab03

---
+++ Lab03/Lab03_QLSinhvien/Nguoi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab03_QuanLySinhVienOOP
+{
+    /// Lớp cha, đại diện cho một con người nói chung.
+    /// SinhVien sẽ kế thừa từ lớp này.
+    public class Nguoi
+    {
+        public string HoTen { get; set; }
+        public DateTime NgaySinh { get; set; }
+
+        public Nguoi()
+        {
+        }
+
+        public Nguoi(string hoTen, DateTime ngaySinh)
+        {
+            HoTen = hoTen;
+            NgaySinh = ngaySinh;
+        }
+
+        public virtual string LayThongTin()
+        {
+            return $"Họ tên: {HoTen} - Ngày sinh: {NgaySinh:dd/MM/yyyy}";
+        }
+    }
+}
